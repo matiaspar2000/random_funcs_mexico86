65d9ed11a1b240961f86f8bc3710c5df37eec650
matiaspar2000 <matipartarrie@gmail.com>
2021-06-09 15:20:16 -0300

    serializaciones

---
+++ SO_funcs/serializaciones.c
@@ -0,0 +1,74 @@
+typedef enum
+{
+	PCB_MENSAJE,
+	TCB_MENSAJE,
+	CAMBIO_ESTADO_MENSAJE,
+	PEDIR_SIGUIENTE_TAREA,
+	MENSAJE,
+    REPORTE_BITACORA,
+    DESPLAZAMIENTO
+    HACER_TAREA
+}op_code;
+
+t_buffer* serilizar_reporte_bitacora(uint32_t id, char* reporte)
+{
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+	void* stream = malloc(sizeof(uint32_t) + sizeof(uint32_t) + strlen(reporte) + 1);
+	int desplazamiento = 0;
+
+	memcpy(stream + desplazamiento, &id, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	void* reporte_len = malloc(sizeof(uint32_t));
+	reporte_len = strlen(reporte) + 1;
+	memcpy(stream + desplazamiento, (void*)(&reporte_len), sizeof(uint32_t));
+	desplazamiento += sizeof(strlen(reporte) + 1);
+	memcpy(stream + desplazamiento, reporte, strlen(reporte) + 1);
+	desplazamiento += strlen(reporte) + 1;
+
+	buffer->size = desplazamiento;
+	buffer->stream = stream;
+	return buffer;
+}
+
+t_buffer* serilizar_desplazamiento(uint32_t tid, uint32_t x_nuevo, uint32_t y_nuevo)
+{
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+	void* stream = malloc(sizeof(uint32_t)*3;
+	int desplazamiento = 0;
+
+	memcpy(stream + desplazamiento, &tid, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	memcpy(stream + desplazamiento, &x_nuevo, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	memcpy(stream + desplazamiento, &y_nuevo, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	buffer->size = desplazamiento;
+	buffer->stream = stream;
+	return buffer;
+}
+
+t_buffer* serilizar_hacer_tarea(uint32_t cantidad, char* tarea)
+{
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+	void* stream = malloc(sizeof(uint32_t) + sizeof(uint32_t) + strlen(tarea) + 1);
+	int desplazamiento = 0;
+
+	memcpy(stream + desplazamiento, &cantidad, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	void* tarea_len = malloc(sizeof(uint32_t));
+	tarea_len = strlen(tarea) + 1;
+	memcpy(stream + desplazamiento, (void*)(&tarea_len), sizeof(uint32_t));
+	desplazamiento += sizeof(strlen(tarea) + 1);
+	memcpy(stream + desplazamiento, tarea, strlen(tarea) + 1);
+	desplazamiento += strlen(tarea) + 1;
+
+	buffer->size = desplazamiento;
+	buffer->stream = stream;
+	return buffer;
+}
+
